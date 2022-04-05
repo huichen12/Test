f4e65b6a880e02f14d870b6d663eb39c746e7818
huichen12 <82217752+huichen12@users.noreply.github.com>
2022-04-05 17:26:46 +0800

    基于邻接表的深度优先搜索（图）

--- Week03/D.c
+++ Week03/D.c
@@ -1,61 +1,62 @@
+//åŸºäºé‚»æ¥è¡¨çš„æ·±åº¦ä¼˜å…ˆæœç´¢ï¼ˆå›¾ï¼‰
 #include <stdio.h>
 #include <stdlib.h>
-#define VertexType char //¶¥µãµÄÊı¾İÀàĞÍ£¨char£© 
-#define VertexMax 20 //×î´ó¶¥µã¸öÊı 
-int visited[VertexMax];//ÉèÖÃÈ«¾ÖÊı×é£¬¼ÇÂ¼±ê¼Ç¶¥µãÊÇ·ñ±»·ÃÎÊ¹ı
-typedef struct ArcNode//±ß±í 
+#define VertexType char //é¡¶ç‚¹çš„æ•°æ®ç±»å‹ï¼ˆcharï¼‰ 
+#define VertexMax 20 //æœ€å¤§é¡¶ç‚¹ä¸ªæ•° 
+int visited[VertexMax];//è®¾ç½®å…¨å±€æ•°ç»„ï¼Œè®°å½•æ ‡è®°é¡¶ç‚¹æ˜¯å¦è¢«è®¿é—®è¿‡
+typedef struct ArcNode//è¾¹è¡¨ 
 {
-    int adjvex;//´æ´¢µÄÊÇ¸Ã¶¥µãÔÚ¶¥µãÊı×é¼´AdjList[]ÖĞµÄÎ»ÖÃ 
+    int adjvex;//å­˜å‚¨çš„æ˜¯è¯¥é¡¶ç‚¹åœ¨é¡¶ç‚¹æ•°ç»„å³AdjList[]ä¸­çš„ä½ç½® 
     struct ArcNode* nextarc;
 }ArcNode;
  
-typedef struct VNode //µ¥¸ö¶¥µã 
+typedef struct VNode //å•ä¸ªé¡¶ç‚¹ 
 {
     char data;
     struct ArcNode* firstarc;
 }VNode;
  
-typedef struct //¶¥µã±í 
+typedef struct //é¡¶ç‚¹è¡¨ 
 {
-    VNode AdjList[VertexMax];//ÓÉ¶¥µã¹¹³ÉµÄ½á¹¹ÌåÊı×é 
-    int vexnum, arcnum; //¶¥µãÊıºÍ±ßÊı 
-    int kind; //¼ÇÂ¼Í¼µÄÀàĞÍ 
+    VNode AdjList[VertexMax];//ç”±é¡¶ç‚¹æ„æˆçš„ç»“æ„ä½“æ•°ç»„ 
+    int vexnum, arcnum; //é¡¶ç‚¹æ•°å’Œè¾¹æ•° 
+    int kind; //è®°å½•å›¾çš„ç±»å‹ 
 }ALGraph;
 void CreateDG(ALGraph* G)
 {
     int i, j;
     int flag;
-    //1.ÊäÈë¶¥µãÊıºÍ±ßÊı 
+    //1.è¾“å…¥é¡¶ç‚¹æ•°å’Œè¾¹æ•° 
     scanf("%d%d", &G->vexnum, &G->arcnum);
     scanf("%d", &flag);
     getchar();
-    //2.¶¥µã±íÊı¾İÓòÌîÖµ³õÊ¼»¯¶¥µã±íÖ¸ÕëÓò 
+    //2.é¡¶ç‚¹è¡¨æ•°æ®åŸŸå¡«å€¼åˆå§‹åŒ–é¡¶ç‚¹è¡¨æŒ‡é’ˆåŸŸ 
     for (i = 0; i < G->vexnum; i++)
     {
         scanf("%c", &G->AdjList[i].data);
         G->AdjList[i].firstarc = NULL;
         getchar();
     }
-    //3.ÊäÈë±ßĞÅÏ¢¹¹ÔìÁÚ½Ó±í 
+    //3.è¾“å…¥è¾¹ä¿¡æ¯æ„é€ é‚»æ¥è¡¨ 
     int v1, v2;
     ArcNode* p1, * p2;
     for (i = 0; i < G->arcnum; i++)
-    {   //ÊäÈë±ßĞÅÏ¢£¬²¢È·¶¨v1ºÍv2ÔÚGÖĞµÄÎ»ÖÃ£¬¼´¶¥µãÔÚAdjList[]Êı×éÖĞµÄÎ»ÖÃ£¨ÏÂ±ê£©  
+    {   //è¾“å…¥è¾¹ä¿¡æ¯ï¼Œå¹¶ç¡®å®šv1å’Œv2åœ¨Gä¸­çš„ä½ç½®ï¼Œå³é¡¶ç‚¹åœ¨AdjList[]æ•°ç»„ä¸­çš„ä½ç½®ï¼ˆä¸‹æ ‡ï¼‰  
         scanf("%d%d", &v1, &v2);
         if (flag == 0) {
             p1 = (ArcNode*)malloc(sizeof(ArcNode));
-            p1->adjvex = v2;//ÌîÉÏ×ø±ê 
-            p1->nextarc = G->AdjList[v1].firstarc;//¸ÄÁ´£¨Í·²å·¨£©  
+            p1->adjvex = v2;//å¡«ä¸Šåæ ‡ 
+            p1->nextarc = G->AdjList[v1].firstarc;//æ”¹é“¾ï¼ˆå¤´æ’æ³•ï¼‰  
             G->AdjList[v1].firstarc = p1;
-            p2 = (ArcNode*)malloc(sizeof(ArcNode));//ÎŞÏòÍ¼µÄ¶Ô³Æ 
+            p2 = (ArcNode*)malloc(sizeof(ArcNode));//æ— å‘å›¾çš„å¯¹ç§° 
             p2->adjvex = v1;
             p2->nextarc = G->AdjList[v2].firstarc;
             G->AdjList[v2].firstarc = p2;
         }
         else {
             p1 = (ArcNode*)malloc(sizeof(ArcNode));
-            p1->adjvex = v2;//ÌîÉÏ×ø±ê 
-            p1->nextarc = G->AdjList[v1].firstarc;//¸ÄÁ´£¨Í·²å·¨£©  
+            p1->adjvex = v2;//å¡«ä¸Šåæ ‡ 
+            p1->nextarc = G->AdjList[v1].firstarc;//æ”¹é“¾ï¼ˆå¤´æ’æ³•ï¼‰  
             G->AdjList[v1].firstarc = p1;
         }
     }
@@ -71,16 +72,16 @@ void DFS(ALGraph G, int v) {
         p = p->nextarc;
     }
 }
-//Éî¶ÈÓÅÏÈËÑË÷
+//æ·±åº¦ä¼˜å…ˆæœç´¢
 void DFSTraverse(ALGraph G) {
     int v;
-    //½«ÓÃ×ö±ê¼ÇµÄvisitÊı×é³õÊ¼»¯Îª0
+    //å°†ç”¨åšæ ‡è®°çš„visitæ•°ç»„åˆå§‹åŒ–ä¸º0
     for (v = 0; v < G.vexnum; ++v) {
         visited[v] = 0;
     }
-    //¶ÔÓÚÃ¿¸ö±ê¼ÇÎªfalseµÄ¶¥µãµ÷ÓÃÉî¶ÈÓÅÏÈËÑË÷º¯Êı
+    //å¯¹äºæ¯ä¸ªæ ‡è®°ä¸ºfalseçš„é¡¶ç‚¹è°ƒç”¨æ·±åº¦ä¼˜å…ˆæœç´¢å‡½æ•°
     for (v = 0; v < G.vexnum; v++) {
-        //Èç¹û¸Ã¶¥µãµÄ±ê¼ÇÎ»Îªfalse£¬Ôòµ÷ÓÃÉî¶ÈÓÅÏÈËÑË÷º¯Êı
+        //å¦‚æœè¯¥é¡¶ç‚¹çš„æ ‡è®°ä½ä¸ºfalseï¼Œåˆ™è°ƒç”¨æ·±åº¦ä¼˜å…ˆæœç´¢å‡½æ•°
         if (!visited[v]) {
             DFS(G, v);
         }
